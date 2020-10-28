8f7767ae39d392802b34581e01a9d9ca475e6804
Staszek <s.zawadzki@visuality.pl>
2020-10-28 11:08:54 +0100

    CORS configuration

---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+CORS_ORIGINS = ENV['CORS_ORIGINS'].to_s.split(', ').presence || '*'
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins CORS_ORIGINS
+    resource '*',
+      headers: :any,
+      expose: %w(Access-Token Client Expiry Token-Type Uid User-Role),
+      methods: %i(get post put patch delete options head)
+  end
+end
