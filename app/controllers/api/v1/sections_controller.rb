796dd37f73ae4b0c112950e4804997e920e2e80f
Staszek <s.zawadzki@visuality.pl>
2022-12-06 20:04:36 +0100

    Simplify Sections

--- app/controllers/api/v1/sections_controller.rb
+++ app/controllers/api/v1/sections_controller.rb
@@ -2,25 +2,13 @@ module Api
   module V1
     class SectionsController < Api::V1::BaseController
       def index
-        sections = Section.includes([:subsections,
-                                     :locations,
-                                     :utilities,
-                                     :images,
-                                     :trivia])
-        options = {
-          include: [:subsections, :locations, :utilities]
-        }
-
-        render json: SectionSerializer.new(sections, options).serializable_hash
+        sections = Section.includes(:images)
+        render json: SectionSerializer.new(sections).serializable_hash
       end
 
       def show
         section = Section.find(params[:id])
-        options = {
-          include: [:subsections, :locations, :utilities, :images, :trivia]
-        }
-
-        render json: SectionSerializer.new(section, options).serializable_hash
+        render json: SectionSerializer.new(section).serializable_hash
       end
     end
   end
--- app/serializers/section_serializer.rb
+++ app/serializers/section_serializer.rb
@@ -1,10 +1,9 @@
 class SectionSerializer
   include JSONAPI::Serializer
 
-  has_many :subsections
-  has_many :locations
-  has_many :utilities
-  has_many :trivia
+  #has_many :locations
+  #has_many :utilities
+  #has_many :trivia
   has_many :images
 
   attributes :id, :start, :finish, :description, :order, :variation,
